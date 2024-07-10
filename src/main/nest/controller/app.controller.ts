32ac754783e75f68f31d2905d51bc5cfed2a96d4
dshuais <1137896420@qq.com>
2024-07-10 14:21:12 +0800

    ✨ feat: 新增错误处理

--- src/main/nest/controller/app.controller.ts
+++ src/main/nest/controller/app.controller.ts
@@ -5,7 +5,13 @@
  * @LastEditTime: 2024-07-06 13:30:39
  * @Description: Controller
  */
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query
+} from '@nestjs/common';
 import { AppService } from '../service/app.service';
 
 @Controller()
@@ -18,7 +24,19 @@ export class AppController {
   }
 
   @Get('api')
-  getApi(): object {
-    return this.appService.getApi();
+  getApi(@Query() query: { err: string }): object {
+    console.log(query);
+    if (query.err === '1') {
+      return new HttpException(
+        {
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+          msg: 'This is a custom message'
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: 'This is a custom message'
+        }
+      );
+    } else return this.appService.getApi();
   }
 }
